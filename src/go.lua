31c4efb9c96a803245629c438b675fc8ebd1dfdf
iggyvolz <iggyvolz@gmail.com>
2014-12-13 00:04:37 -0500

    Added more specific URL-checking

--- src/go.lua
+++ src/go.lua
@@ -1,5 +1,5 @@
 local self=function(r)
-  if r.uri:sub(1,7)=="/forums" then return apache2.DECLINED end --Ignore forums
+  if r.uri:sub(1,8)=="/forums/" or r.uri:sub(1,8)=="/forums?" or r.uri=="/forums" then return apache2.DECLINED end --Ignore forums
   r:puts(require "pl.pretty".write(tbl))
   return apache2.DONE
 end
