c106d2647951e297c87da169f296cc77b0d98821
iggyvolz <iggyvolz@gmail.com>
2014-12-12 23:38:49 -0500

    Screw phpMyAdmin

--- src/go.lua
+++ src/go.lua
@@ -1,6 +1,5 @@
 local self=function(r)
   if r.uri:sub(1,7)=="/forums" then return apache2.DECLINED end --Ignore forums
-  if r.uri:sub(1,4)=="/pma" then return apache2.DECLINED end -- Ignore phpmyadmin
   r:puts(require "pl.pretty".write(tbl))
   return apache2.DONE
 end
