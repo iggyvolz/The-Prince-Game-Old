e0bc89d9e1c7b2ce2c9757ef71ee84be718ba055
iggyvolz <iggyvolz@gmail.com>
2014-12-12 22:33:17 -0500

    Fix

--- src/go.lua
+++ src/go.lua
@@ -1,6 +1,5 @@
 local self=function(r)
-  r:puts(r.uri:sub(1,-5))
-  if r.uri:sub(1,-5)=="/forums" then return apache2.DECLINED end --Ignore forums
+  if r.uri:sub(1,7)=="/forums" then return apache2.DECLINED end --Ignore forums
   r:puts(require "pl.pretty".write(tbl))
   return apache2.DONE
 end
