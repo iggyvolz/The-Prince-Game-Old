ddcbc150dcc17046dc626b096907cb90b3f03300
iggyvolz <iggyvolz@gmail.com>
2014-12-12 22:30:27 -0500

    Typo: url => uri

--- src/go.lua
+++ src/go.lua
@@ -1,5 +1,5 @@
 local self=function(r)
-  if r.url:sub(1,-5)=="/forums" then return apache2.DECLINED end --Ignore forums
+  if r.uri:sub(1,-5)=="/forums" then return apache2.DECLINED end --Ignore forums
   r:puts(require "pl.pretty".write(tbl))
   return apache2.DONE
 end
