0e2f6fcb8bd1256655615b55c105165e39416a6e
iggyvolz <iggyvolz@gmail.com>
2014-12-13 22:36:24 -0500

    Update remap for forums

--- remap.lua
+++ remap.lua
@@ -1,5 +1,5 @@
 function remap(r)
-  if r.uri:sub(1,8)=="/forums/" or r.uri:sub(1,8)=="/forums?" or r.uri=="/forums" then return apache2.OK end --Ignore forums
+  if r.uri:sub(1,8)=="/forums/" or r.uri:sub(1,8)=="/forums?" or r.uri=="/forums" then r.filename="/usr/local/The-Prince-Game"..r.uri return apache2.OK end --Ignore forums
   r.filename="/usr/local/The-Prince-Game/go.lua"
   return apache2.OK
 end
