a270726b9530613a5cda824973ab440b62f9d4ec
iggyvolz <iggyvolz@gmail.com>
2014-12-20 17:08:17 -0500

    Copy table before scanning

--- src/init.lua
+++ src/init.lua
@@ -2,8 +2,17 @@ return function(tbl)
   return function(r)
     local json=require "json"
     if r:wsupgrade() then
+      local function copy(obj, seen)
+        if type(obj) ~= 'table' then return obj end
+        if seen and seen[obj] then return seen[obj] end
+        local s = seen or {}
+        local res = setmetatable({}, getmetatable(obj))
+        s[obj] = res
+        for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
+        return res
+      end
       local function doscan(_t)
-        local t=_t
+        local t=copy(_t)
         for i in pairs(t) do
           if type(t[i])=="table" then
             t[i]=doscan(t[i])
