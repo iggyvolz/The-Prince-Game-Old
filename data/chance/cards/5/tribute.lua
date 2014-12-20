7b0957c8913b2744f6706f89c0f81f10536e069a
iggyvolz <iggyvolz@aim.com>
2014-12-20 00:14:25 -0500

    Fix luacheck's pickiness

--- data/chance/cards/5/tribute.lua
+++ data/chance/cards/5/tribute.lua
@@ -2,10 +2,11 @@ return function(tbl,states)
   local tribute=0
   local papal=0
   local excluded={"england","ottoman_empire","venice","egypt","morocco","papal_states","naples"}
-  for i,#states do
+  for i=1,#states do
     tribute=tribute+tbl.data.principalities[states[i]].tribute
     if not excluded[states[i]] then papal=tribute+(0.1*tbl.data.principalities[states[i]].tribute) end
   end
   papal=math.ceil(papal/100)*100
   tribute=tribute-papal
+  return papal,tribute -- TODO when I figure out money hook
 end
