d2f72629c97f047eb11a14c7ca1ba49493faf91a
iggyvolz <iggyvolz@gmail.com>
2014-12-20 12:11:22 -0500

    Agatholes' tribute is 300

--- data/chance/cards/1/draw.lua
+++ data/chance/cards/1/draw.lua
@@ -1,3 +1,5 @@
 return function(tbl)
     tbl.data.principalities.papal_states.rebellion_cards=3
+    tbl.data.principalities.papal_states.old_tribute=tbl.data.principalities.papal_states.tribute
+    tbl.data.principalities.papal_states.tribute=300
 end
--- data/chance/cards/1/text.lua
+++ data/chance/cards/1/text.lua
@@ -1,1 +1,1 @@
-return "Agatholes has taken over Sicily, which is a section of the Papal States.\n\nWhile this card is active, the tribute for the Papal States is lowered to 00, and the owner must draw 3 Rebellion cards instead of 2."
+return "Agatholes has taken over Sicily, which is a section of the Papal States.\n\nWhile this card is active, the tribute for the Papal States is lowered to $300, and the owner must draw 3 Rebellion cards instead of 2."
