165b8b35f94b5f4ab33c3378c834ce0da164a6b7
iggyvolz <iggyvolz@aim.com>
2014-12-20 00:25:30 -0500

    Missed another end

--- data/chance/cards/7/invade_success.lua
+++ data/chance/cards/7/invade_success.lua
@@ -1,4 +1,4 @@
 return function(tbl,country)
-  if country ~="papal_states" then return false
+  if country ~="papal_states" then return false end
   tbl.data.principalities.papal_states.invasion=500
 end
