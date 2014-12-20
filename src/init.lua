82f311d49eb0f24129718e2de43222b49c9d5473
iggyvolz <iggyvolz@gmail.com>
2014-12-20 11:25:00 -0500

    Pass tbl to ini script

--- src/init.lua
+++ src/init.lua
@@ -1,21 +1,13 @@
-local self=function(r)
-  if r:wsupgrade() then
-    -- Then, we'll write something to the client:
-    r:wswrite("Hello, world! Please send me something.")
-
-    -- Receive a line (frame) from the client:
-    local line = r:wsread() or "nothing"
-
-    -- Echo that line back to the client
-    r:wswrite("You wrote: " .. line)
-
-    -- Close up the connection
-    r:wswrite("Goodbye!")
-    r:wsclose()
-  else
-    r.status=405
-    r:puts("405 error - Please use webhooks.")
+return function(tbl)
+  return function(r)
+    if r:wsupgrade() then
+      local foo=tbl
+      r:wswrite(require "pl.pretty".write(foo))
+      r:wsclose()
+    else
+      r.status=405
+      r:puts("405 error - Please use webhooks.")
+    end
+    return apache2.DONE
   end
-  return apache2.DONE
 end
-return self
