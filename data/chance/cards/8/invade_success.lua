60003c25bd72c7d83a03cf6148acaca205db2dfa
iggyvolz <iggyvolz@aim.com>
2014-12-20 00:44:16 -0500

    Add option to use mercenaries

--- data/chance/cards/8/invade_success.lua
+++ data/chance/cards/8/invade_success.lua
@@ -1,6 +1,5 @@
-return function(tbl,country)
-  if "we used mercenaries" then tbl.data.principalities[country].rebellion_cards=4 end
+return function(tbl,country,used_mercenaries)
+  if used_mercenaries then tbl.data.principalities[country].rebellion_cards=4 end
 end
 
 -- TODO remove limit when country lost or invaded.
--- TODO add option to use mercenaries
