fb6d904a6a23964a861d6ec46480d6eda1e35e76
iggyvolz <iggyvolz@aim.com>
2014-12-20 00:31:35 -0500

    Add card 8 with pseudo-code and TODO's

---
+++ data/chance/cards/8/invade.lua
@@ -0,0 +1,7 @@
+return function(tbl,country,level)
+  tbl
+  country
+  return level+1
+end
+
+-- TODO decrease money here
---
+++ data/chance/cards/8/invade_success.lua
@@ -0,0 +1,6 @@
+return function(tbl,country)
+  if "we used mercenaries" then tbl.data.principalities[country].rebellion_cards=4 end
+end
+
+-- TODO remove limit when country lost or invaded.
+-- TODO add option to use mercenaries
