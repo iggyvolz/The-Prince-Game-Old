1889f8b772fe38b1ba3fddd576846da3da8475f4
iggyvolz <iggyvolz@aim.com>
2014-12-20 00:12:48 -0500

    Missed an end

--- data/chance/cards/5/tribute.lua
+++ data/chance/cards/5/tribute.lua
@@ -4,7 +4,7 @@ return function(tbl,states)
   local excluded={"england","ottoman_empire","venice","egypt","morocco","papal_states","naples"}
   for i,#states do
     tribute=tribute+tbl.data.principalities[states[i]].tribute
-    if not excluded[states[i]] then papal=tribute+(0.1*tbl.data.principalities[states[i]].tribute)
+    if not excluded[states[i]] then papal=tribute+(0.1*tbl.data.principalities[states[i]].tribute) end
   end
   papal=math.ceil(papal/100)*100
   tribute=tribute-papal
