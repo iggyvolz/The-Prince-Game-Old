183fdf2536877b5b21fc3abdb2369cd7c0f7daa6
iggyvolz <iggyvolz@gmail.com>
2014-12-20 19:48:31 -0500

    Trace if we're not on server

--- src/log.lua
+++ src/log.lua
@@ -1,48 +1,54 @@
-return function(l)
-  local json=require "json"
-  local function exists(f)
-    local h=io.open(f,"r")
-    if h then h:close() return true else return false end
-  end
-  local path="/var/log/the_prince/"
-  local num=1
-  while exists(path..num) do
-    num=num+1
-  end
-  local globals={
-    ["phpbb_root_path"]="/usr/local/The-Prince-Game/forums/",
-    ["phpEx"]="php",
-    ["mode"]="post",
-    ["subject"]="Log #"..num,
-    ["message"]=l,
-    ["username"]="The_Prince_Bot",
-    ["uid"]=48,
-    ["bitfield"]="",
-    ["options"]="",
-    ["topic_type"]=0,
-    ["poll"]=nil,
-    ["data"]={
-      ["forum_id"]=7,
-      ["topic_title"]="Log #"..num,
-      ["icon_id"]=false,
-      ["enable_bbcode"]=false,
-      ["enable_smilies"]=false,
-      ["enable_urls"]=false,
-      ["enable_sig"]=false,
+if _ENV.is_production then
+  return function(l)
+    local json=require "json"
+    local function exists(f)
+      local h=io.open(f,"r")
+      if h then h:close() return true else return false end
+    end
+    local path="/var/log/the_prince/"
+    local num=1
+    while exists(path..num) do
+      num=num+1
+    end
+    local globals={
+      ["phpbb_root_path"]="/usr/local/The-Prince-Game/forums/",
+      ["phpEx"]="php",
+      ["mode"]="post",
+      ["subject"]="Log #"..num,
       ["message"]=l,
-      ["message_md5"]=nil,
-      ["bbcode_bitfield"]=nil,
-      ["bbcode_uid"]=nil,
-      ["post_edit_locked"]=0,
-      ["topic_title"]="Log #"..num,
-      ["notify_set"]=false,
-      ["notify"]=false,
-      ["post_time"]=0,
-      ["enable_indexing"]=true,
-      ["force_visibility"]=true
-    },
-    ["sql"]="SELECT * FROM phpbb_users WHERE user_id = 48"
-  }
-  io.open(path..num, "w"):write(json.encode(globals)):close()
-  io.popen("php /usr/local/The-Prince-Game/log.php "..path..num)
+      ["username"]="The_Prince_Bot",
+      ["uid"]=48,
+      ["bitfield"]="",
+      ["options"]="",
+      ["topic_type"]=0,
+      ["poll"]=nil,
+      ["data"]={
+        ["forum_id"]=7,
+        ["topic_title"]="Log #"..num,
+        ["icon_id"]=false,
+        ["enable_bbcode"]=false,
+        ["enable_smilies"]=false,
+        ["enable_urls"]=false,
+        ["enable_sig"]=false,
+        ["message"]=l,
+        ["message_md5"]=nil,
+        ["bbcode_bitfield"]=nil,
+        ["bbcode_uid"]=nil,
+        ["post_edit_locked"]=0,
+        ["topic_title"]="Log #"..num,
+        ["notify_set"]=false,
+        ["notify"]=false,
+        ["post_time"]=0,
+        ["enable_indexing"]=true,
+        ["force_visibility"]=true
+      },
+      ["sql"]="SELECT * FROM phpbb_users WHERE user_id = 48"
+    }
+    io.open(path..num, "w"):write(json.encode(globals)):close()
+    io.popen("php /usr/local/The-Prince-Game/log.php "..path..num)
+  end
+else
+  return function(l)
+    apache2.trace1(l)
+  end
 end
