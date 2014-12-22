619aec3c1f39007fc19f9d9ce12c5134ffb59848
iggyvolz <iggyvolz@gmail.com>
2014-12-22 14:15:03 -0500

    Added initial actions structure

--- src/init.lua
+++ src/init.lua
@@ -27,11 +27,20 @@ return function(tbl)
         t.src=nil
         return doscan(t)
       end
-      local conts=json.encode(scan(tbl))
-      r:wswrite(conts)
-      tbl.log=tbl.log.."SERVER: "..conts.."\n\n"
-      local get=r:wsread() or nil
-      tbl.log=tbl.log.."CLIENT: "..get.."\n\n"
+      r:wswrite(json.encode({["action"]="INIT",["data"]=scan(tbl)}))
+      local get=json.decode(r:wsread()) or nil
+      while get do
+        if tbl.src.actions[get.action] then
+          local conts,err=tbl.src.actions[get.action].run(get.data)
+          tbl.log=tbl.log.."\n\nSERVER: "..json.encode(conts)
+          if err then tbl.log=tbl.log.."\n\nERROR: "..json.encode(err) end
+          r:wswrite(json.encode({["action"]=get.action,["data"]=conts,["err"]=err}))
+          if err then if err.fatal then tbl.log=tbl.log.."\n\nFATAL ERROR - CLOSING CONNECTION" end end
+        else
+          tbl.log=tbl.log.."\n\nACTION NOT RECOGNIZED: \""..get.action.."\" - CLOSING CONNECTION"
+        end
+        get=json.decode(r:wsread()) or nil
+      end
       r:wsclose()
       tbl.src.log(tbl.log)
     else
