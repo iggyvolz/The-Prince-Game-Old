1f2237c62ef9a3a94d3bf4360271102f041b5c88
iggyvolz <iggyvolz@gmail.com>
2014-12-27 14:12:08 -0500

    Don't concatenate nil

--- src/actions/header.lua
+++ src/actions/header.lua
@@ -1,5 +1,5 @@
 return function(tbl,data)
-  if not data then tbl.log=tbl.log.."\n\nINVALID CLIENT "..data return "ERROR",{["fatal"]=true,["code"]=3,["msg"]="INVALID CLIENT"} end
+  if not data then tbl.log=tbl.log.."\n\nINVALID CLIENT" return "ERROR",{["fatal"]=true,["code"]=3,["msg"]="INVALID CLIENT"} end
   tbl.client=data
   tbl.log=tbl.log.."\n\nUSING CLIENT "..data
   return "OK",nil
