8f0297e9ccfe79c0b7481328b1445ae835f62271
iggyvolz <iggyvolz@gmail.com>
2014-12-30 14:01:32 -0500

    Adds bans from hipchat *braces for spam*

---
+++ ban.php
@@ -0,0 +1,43 @@
+<?php
+$banlist=explode(",",str_replace("\"","",explode("{",explode("}\n end\ntbl.bans=temp()",file_get_contents("go.lua"))[0])[count(explode("{",explode("}\n end\ntbl.bans=temp()",file_get_contents("go.lua"))[0]))-1]));
+$msg=$argv[1];
+//$msg=json_decode(file_get_contents("php://input"))->item->message->message;
+$user=explode("@banish ",$msg)[1];
+if(in_array($user,$banlist))
+{
+  $user=htmlspecialchars($user);
+  $data="User $user already banned";
+  $color="red";
+}
+else
+{
+  $expl=explode("}\n end\ntbl.bans=temp()",file_get_contents("go.lua"));
+  $conts=$expl[0].",\"$user\"}\n end\ntbl.bans=temp()".$expl[1];
+  file_put_contents("go.lua",$conts);
+  $user=htmlspecialchars($user);
+  $data="User $user has been banned";
+  $color="green";
+}
+define("ROOM_NAME","The Prince");
+define("ROOM_ID",1052690);
+define("TOKEN","6fEaJ9afkF23XndQnWE4thaOAAUZldKF0hw7LaiR");
+list($rid,$token)=[ROOM_ID,TOKEN];
+$url = "https://api.hipchat.com/v2/room/$rid/notification?auth_token=$token";
+$options = array(
+  'auth_token' => $token,
+  'color' => $color,
+  'message' => "<pre>$data</pre>",
+  'notify' => true,
+  'message_format' => 'html'
+);
+$options = json_encode($options);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $options);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  'Content-Type: application/json',
+  'Content-Length: ' . strlen($options))
+);
+$var = curl_exec($ch);
+?>
--- remap.lua
+++ remap.lua
@@ -3,6 +3,7 @@ function remap(r)
   if r.uri=="/" or r.uri:sub(1,2)=="/?" then r.filename="/usr/local/The-Prince-Game/index.html" return apache2.OK end  if r.uri=="/" or r.uri:sub(1,2)=="/?" then r.filename="/usr/local/The-Prince-Game/index.html" return apache2.OK end
   if r.uri=="/api" then r.filename="/usr/local/The-Prince-Game/go.lua" return apache2.OK end
   if r.unparsed_uri=="/banlist?p=ucn9aVcQ864dk42uyrBecZ4Po" then r.filename="/usr/local/The-Prince-Game/banlist.php" return apache2.OK end
+  if r.unparsed_uri=="/ban?p=ucn9aVcQ864dk42uyrBecZ4Po" then r.filename="/usr/local/The-Prince-Game/ban.php" return apache2.OK end
   r.filename="/usr/local/The-Prince-Game/404.lua"
   r.status=404
   return apache2.OK
