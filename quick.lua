9f6ce4b3f339f2602039e5e4a5cdbaf3392b7c7d
iggyvolz <iggyvolz@gmail.com>
2015-01-05 16:45:53 -0500

    Fix debugging again

--- quick.lua
+++ quick.lua
@@ -1,5 +1,5 @@
 function quick(r)
-  r.debug(r.uri)
+  r:debug(r.uri)
   if r.uri:sub(1,34)=="https://www.theprincegame.com/api/" then r.headers_out={"HTTP/1.1 301 Moved Permanently","Location: https://theprincegame.com/api/"..r.uri:sub(35)} return apache2.DONE end
   if r.uri:sub(1,26)=="https://theprincegame.com/" and r.uri:sub(1,30) ~= "https://theprincegame.com/api/" then r.headers_out={"HTTP/1.1 301 Moved Permanently","Location: https://www.theprincegame.com/"..r.uri:sub(27)} return apache2.DONE end
   return apache2.DECLINED
