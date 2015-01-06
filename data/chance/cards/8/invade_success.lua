5cdc1f2290addedceb26bab1af733d30f0d70008
iggyvolz <iggyvolz@aim.com>
2015-01-06 18:20:02 +0000

    Fix #3 with psuedo-code

--- data/chance/cards/8/invade_success.lua
+++ data/chance/cards/8/invade_success.lua
@@ -1,5 +1,8 @@
 return function(tbl,country,used_mercenaries)
-  if used_mercenaries then tbl.data.principalities[country].rebellion_cards=4 end
+  if not used_mercenaries then return end
+  tbl.data.principalities[country].rebellion_cards=4
+  table.insert(tbl.data.principalities[country].hooks.invade_success,function(self)
+    tbl.data.principalities[country].rebellion_cards=4
+    self=nil
+  end)
 end
-
--- TODO remove limit when country lost or invaded.
