73a13da92abc6ce247e0a742890551ea8014fd03
iggyvolz <iggyvolz@aim.com>
2015-01-06 18:23:25 +0000

    Fix build that probably just failed

--- data/chance/cards/8/invade_success.lua
+++ data/chance/cards/8/invade_success.lua
@@ -1,8 +1,8 @@
 return function(tbl,country,used_mercenaries)
   if not used_mercenaries then return end
   tbl.data.principalities[country].rebellion_cards=4
-  table.insert(tbl.data.principalities[country].hooks.invade_success,function(self)
+  table.insert(tbl.data.principalities[country].hooks.invade_success,function(n)
     tbl.data.principalities[country].rebellion_cards=4
-    self=nil
+    tbl.data.principalities[country].hooks.invade_success[n]=nil
   end)
 end
