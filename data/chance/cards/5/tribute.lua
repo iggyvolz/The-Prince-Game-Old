bdd33568e57aabafc2daf5bdbffff893c286903d
iggyvolz <iggyvolz@aim.com>
2015-01-08 18:13:53 +0000

    Hook money hook into tribute

--- data/chance/cards/5/tribute.lua
+++ data/chance/cards/5/tribute.lua
@@ -1,12 +1,20 @@
-return function(tbl,states)
+return function(tbl,player)
   local tribute=0
-  local papal=0
+  local papal_tribute=0
   local excluded={"england","ottoman_empire","venice","egypt","morocco","papal_states","naples"}
-  for i=1,#states do
-    tribute=tribute+tbl.data.principalities[states[i]].tribute
-    if not excluded[states[i]] then papal=tribute+(0.1*tbl.data.principalities[states[i]].tribute) end
+  for i=1,#tbl.data.players[player].states do
+    tribute=tribute+tbl.data.principalities[tbl.data.players[player].states[i]].tribute
+    if not excluded[tbl.data.players[player].states[i]] then papal_tribute=tribute+(0.1*tbl.data.principalities[tbl.data.players[player].states[i]].tribute) end
   end
-  papal=math.ceil(papal/100)*100
-  tribute=tribute-papal
-  return papal,tribute -- TODO when I figure out money hook
+  papal_tribute=math.ceil(papal_tribute/100)*100
+  tribute=tribute-papal_tribute
+  tbl.data.players[player].money=tbl.data.players[player].money+tribute
+  local papal_owner
+  for i=1,4 do
+    for j=1,#tbl.dta.players[i].states do
+      if tbl.data.players[i].states[j].name=="papal_states" then papal_owner=i end
+    end
+  end
+  if papal_owner then tbl.data.players[papal_owner].money=tbl.data.players[papal_owner].money+papal_tribute end
+  return false
 end
