e98fc2064d10cde8fe583f6d33e930972bd39508
iggyvolz <iggyvolz@gmail.com>
2015-01-10 13:38:46 -0500

    Re-add to-www shift

--- quick.lua
+++ quick.lua
@@ -1,5 +1,5 @@
 function quick()
   --if r.hostname=="www.theprincegame.com" and r.uri:sub(1,4)=="/api" then r:puts("<head><meta http-equiv=\"refresh\" content=\"0;url=https://theprincegame.com/api/"..r.unparsed_uri.."\"></head>") return apache2.DONE end
-  --if r.hostname=="theprincegame.com" and r.uri:sub(1,4)~="/api" then r:puts("<head><meta http-equiv=\"refresh\" content=\"0;url=https://www.theprincegame.com"..r.unparsed_uri.."\"></head>") return apache2.DONE end
+  if r.hostname=="theprincegame.com" and r.uri:sub(1,4)~="/api" then r:puts("<head><meta http-equiv=\"refresh\" content=\"0;url=https://www.theprincegame.com"..r.unparsed_uri.."\"></head>") return apache2.DONE end
   return apache2.DECLINED
 end
