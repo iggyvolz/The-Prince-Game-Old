2a74501c2e30cb27e379cc06b9e9be1aee76dd92
iggyvolz <iggyvolz@aim.com>
2015-04-03 10:41:18 -0400

    Add mercenary to invade hook

--- data/chance/cards/7/invade_success.lua
+++ data/chance/cards/7/invade_success.lua
@@ -1,4 +1,4 @@
 return function(country)
-  if country ~="papal_states" then return false end
+  if country ~="papal_states" then return end
   data.principalities.papal_states.invasion=500
 end
--- data/chance/cards/8/invade_success.lua
+++ data/chance/cards/8/invade_success.lua
@@ -1,8 +1,8 @@
 return function(country,used_mercenaries)
   if not used_mercenaries then return end
   data.principalities[country].rebellion_cards=4
-  table.insert(data.principalities[country].hooks.invade_success,function(n)
-    data.principalities[country].rebellion_cards=4
-    data.principalities[country].hooks.invade_success[n]=nil
-  end)
+  data.principalities[country].invade_success=function()
+    data.principalities[country].rebellion_cards=2
+    data.principalities[country].invade_success=nil
+  end
 end
